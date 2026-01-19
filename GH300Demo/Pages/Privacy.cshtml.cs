8adff394fb682dc8af420a771e39908c193b6797
Andrew Conniff <anconnif@microsoft.com>
2026-01-19 17:44:13 +0000

    My New live demo

---
+++ GH300Demo/Pages/Privacy.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+
+namespace GH300Demo.Pages;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string HiringDate { get; set; }
+    public string Department { get; set; }
+    public string JobTitle { get; set; }
+}
+
+public class PrivacyModel : PageModel
+{
+    public List<Employee> Employees { get; set; } = new();
+
+    public void OnGet()
+    {
+        var wwwrootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+        var filePath = Path.Combine(wwwrootPath, "sampledata.json");
+        
+        if (System.IO.File.Exists(filePath))
+        {
+            var json = System.IO.File.ReadAllText(filePath);
+            Employees = JsonSerializer.Deserialize<List<Employee>>(json) ?? new();
+        }
+    }
+}
+
